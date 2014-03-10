5b094b8c03c6987dc81ce88632286ae2888ff578
treefingers <tojr77@gmail.com>
2014-03-10 23:49:20 +1100

    changed addCommand to take a details object + added man command also modified help to display summaries

--- static/js/jurassicSystems.js
+++ static/js/jurassicSystems.js
@@ -12,7 +12,7 @@
 
       api.buildCommandLine = function(line) {
          var commandName = line.trim().split(/ /)[0],
-             command = env.commands[commandName];
+             command = env.commands[commandName] && env.commands[commandName].command;
 
          $('#' + env.active).find('.command-history').append($('<div class="entered-command"/>').text('> ' + line));
 
@@ -23,9 +23,9 @@
          }
       }
 
-      api.addCommand = function(name, command) {
-         if (name && !env.commands.hasOwnProperty(name) && (command.constructor === Function)) {
-            env.commands[name] = command;
+      api.addCommand = function(details) {
+         if (details.name && !env.commands.hasOwnProperty(details.name) && (details.command.constructor === Function)) {
+            env.commands[details.name] = details;
          }
       }
 
@@ -119,152 +119,200 @@
    jpTerminal.init();
    jpTerminal.activeTerminal('main-terminal');
 
-   jpTerminal.addCommand('music', function(env, inputLine) {
-      var arg = inputLine.trim().split(/ +/)[1] || '',
-          output = $('<span/>').text('music: must specify state [on|off]');
+   jpTerminal.addCommand({
+      name: 'music',
+      summary: 'music - turn background music on or off',
+      manPage: '',
+      command: function(env, inputLine) {
+         var arg = inputLine.trim().split(/ +/)[1] || '',
+             output = $('<span/>').text('music: must specify state [on|off]');
 
-      if (!arg || !arg.match(/^(?:on|off)$/i)) {
-         $('#main-input').append(output);
-      } else {
-         if (arg.toLowerCase() === 'on') {
-            if (!env.musicOn) {
-               env.sounds.dennisMusic.play();
+         if (!arg || !arg.match(/^(?:on|off)$/i)) {
+            $('#main-input').append(output);
+         } else {
+            if (arg.toLowerCase() === 'on') {
+               if (!env.musicOn) {
+                  env.sounds.dennisMusic.play();
+               }
+               env.musicOn = true;
+            } else if (arg.toLowerCase() === 'off') {
+               env.sounds.dennisMusic.stop();
+               env.musicOn = false;
             }
-            env.musicOn = true;
-         } else if (arg.toLowerCase() === 'off') {
-            env.sounds.dennisMusic.stop();
-            env.musicOn = false;
          }
       }
    });
 
-   jpTerminal.addCommand('access', function(env, inputLine) {
-      var output = $('<span/>').text('access: PERMISSION DENIED'),
-          arg = inputLine.split(/ +/)[1] || '',
-          magicWord = inputLine.trim().substring(inputLine.lastIndexOf(' '));
+   jpTerminal.addCommand({
+         name: 'access', 
+         summary: 'access - access a target environment on the Jurassic Systems grid',
+         manPage: 'USAGE\n   access [system name]',
+         command: function(env, inputLine) {
+         var output = $('<span/>').text('access: PERMISSION DENIED'),
+             arg = inputLine.split(/ +/)[1] || '',
+             magicWord = inputLine.trim().substring(inputLine.lastIndexOf(' '));
 
-   if (arg === '') {
-      $('#main-input').append($('<span/>').text('access: must specify target system'));
-
-      return;
-   } else if (inputLine.split(' ').length > 2 && magicWord.trim() === 'please') {
-         $('#main-input').append($('<img id="asciiNewman" src="/img/asciiNewman.jpg" />'));
-         $('#asciiNewman').load(function() {
-            $('#' + env.active + ' .inner-wrap').scrollTop($('#' + env.active + ' .inner-wrap')[0].scrollHeight);
-         });
+      if (arg === '') {
+         $('#main-input').append($('<span/>').text('access: must specify target system'));
 
          return;
-      }
+      } else if (inputLine.split(' ').length > 2 && magicWord.trim() === 'please') {
+            $('#main-input').append($('<img id="asciiNewman" src="/img/asciiNewman.jpg" />'));
+            $('#asciiNewman').load(function() {
+               $('#' + env.active + ' .inner-wrap').scrollTop($('#' + env.active + ' .inner-wrap')[0].scrollHeight);
+            });
 
-      $('#main-input').append(output);
-      env.sounds.beep.play();
+            return;
+         }
 
-      if (++env.accessAttempts >= 3) {
-         var andMessage = $('<span/>').text('...and....'),
-             errorSpam;
-
-         $('.irix-window').unbind('keydown');
-         $('#main-prompt').addClass('hide');
-
-         setTimeout(function() {
-            $('#main-input').append(andMessage);
-         }, 200);
-
-         setTimeout(function() {
-            env.sounds.lockDown.play();
-         }, 1000);
-
-         setTimeout(function() {
-            $('#environment').animate({'left': '+=3000'}, 
-               2000, 
-               function() {
-                  setTimeout(function() {
-                     $('#irix-desktop').hide();
-                     if (errorSpam) {
-                        clearInterval(errorSpam);
-                     }
-
-                     $('#the-king-window').ready(function() {
-                        $('#mac-hd-window').css('background-image', 'url(/img/macHDBlur.jpg)');
-                        $('#the-king-window').show();
-                        if ($(window).width() < 1200) {
-                           setTimeout(function() {
-                              $('#home-key').css('z-index', '64000');
-                           }, 10000);
-                        }
-                     });
-                  }, 2000);
-               });
-         }, 4000);
-
-         setTimeout(function() {
-            errorSpam = setInterval(function() {
-               var errorMessage = $('<div>YOU DIDN\'T SAY THE MAGIC WORD!</div>');
-               $('#main-input').append(errorMessage);
-               $('#main-inner').scrollTop($('#main-inner')[0].scrollHeight);
-            }, 50);
-         }, 1000);
-      }
-   });
-
-   jpTerminal.addCommand('system', function(env, inputLine) {
-      var arg = inputLine.split(/ +/)[1] || '',
-          output = '<span>system: must specify target system</span>';
-
-      if (arg.length > 0) {
-         arg = arg.replace(/s$/, '');
-         arg = arg[0].toUpperCase() + arg.slice(1);
-         arg = $('<div/>').text(arg).html();
-         
-         output = '<div>' + arg + ' containment enclosure....</div>' +
-                  '<table id="system-output"><tbody>' +
-                  '<tr><td>Security</td><td>[OK]</td></tr>' +
-                  '<tr><td>Fence</td><td>[OK]</tr>' +
-                  '<tr><td>Feeding Pavilion</td><td>[OK]</td></tr>' +
-                  '</tbody></table>';
-
-         $('#main-prompt').addClass('hide');
-         $('#main-input').append($(output));
-         output = '<div>System Halt!</div>';
+         $('#main-input').append(output);
          env.sounds.beep.play();
 
-         setTimeout(function() {
-            env.sounds.beep.play();
+         if (++env.accessAttempts >= 3) {
+            var andMessage = $('<span/>').text('...and....'),
+                errorSpam;
+
+            $('.irix-window').unbind('keydown');
+            $('#main-prompt').addClass('hide');
+
+            setTimeout(function() {
+               $('#main-input').append(andMessage);
+            }, 200);
+
+            setTimeout(function() {
+               env.sounds.lockDown.play();
+            }, 1000);
+
+            setTimeout(function() {
+               $('#environment').animate({'left': '+=3000'}, 
+                  2000, 
+                  function() {
+                     setTimeout(function() {
+                        $('#irix-desktop').hide();
+                        if (errorSpam) {
+                           clearInterval(errorSpam);
+                        }
+
+                        $('#the-king-window').ready(function() {
+                           $('#mac-hd-window').css('background-image', 'url(/img/macHDBlur.jpg)');
+                           $('#the-king-window').show();
+                           if ($(window).width() < 1200) {
+                              setTimeout(function() {
+                                 $('#home-key').css('z-index', '64000');
+                              }, 10000);
+                           }
+                        });
+                     }, 2000);
+                  });
+            }, 4000);
+
+            setTimeout(function() {
+               errorSpam = setInterval(function() {
+                  var errorMessage = $('<div>YOU DIDN\'T SAY THE MAGIC WORD!</div>');
+                  $('#main-input').append(errorMessage);
+                  $('#main-inner').scrollTop($('#main-inner')[0].scrollHeight);
+               }, 50);
+            }, 1000);
+         }
+      }
+   });
+
+   jpTerminal.addCommand({
+         name: 'system',
+         summary: 'system - check a system\'s current status',
+         manPage: '',
+         command: function(env, inputLine) {
+         var arg = inputLine.split(/ +/)[1] || '',
+             output = '<span>system: must specify target system</span>';
+
+         if (arg.length > 0) {
+            arg = arg.replace(/s$/, '');
+            arg = arg[0].toUpperCase() + arg.slice(1);
+            arg = $('<div/>').text(arg).html();
+            
+            output = '<div>' + arg + ' containment enclosure....</div>' +
+                     '<table id="system-output"><tbody>' +
+                     '<tr><td>Security</td><td>[OK]</td></tr>' +
+                     '<tr><td>Fence</td><td>[OK]</tr>' +
+                     '<tr><td>Feeding Pavilion</td><td>[OK]</td></tr>' +
+                     '</tbody></table>';
+
+            $('#main-prompt').addClass('hide');
             $('#main-input').append($(output));
-            $('#' + env.active + ' .inner-wrap').scrollTop($('#' + env.active + ' .inner-wrap')[0].scrollHeight);
-            $('#main-prompt').removeClass('hide');
-         }, 900);
-      } else {
-         $('#main-input').append($(output));
+            output = '<div>System Halt!</div>';
+            env.sounds.beep.play();
+
+            setTimeout(function() {
+               env.sounds.beep.play();
+               $('#main-input').append($(output));
+               $('#' + env.active + ' .inner-wrap').scrollTop($('#' + env.active + ' .inner-wrap')[0].scrollHeight);
+               $('#main-prompt').removeClass('hide');
+            }, 900);
+         } else {
+            $('#main-input').append($(output));
+         }
       }
    });
 
-   jpTerminal.addCommand('ls', function(env, inputLine) {
-      $('#main-input').append($('<div>zebraGirl.jpg</div>'));
-   });
-
-   jpTerminal.addCommand('display', function(env, inputLine) {
-      var args = inputLine.trim().split(' ');
-
-      if (args.length < 2) {
-         $('#main-input').append($('<span>display: no file specified</span>'));
-         return;
-      }
-
-      if (inputLine.match(/zebraGirl\.jpg/)) {
-         setTimeout(function() {
-            $('#zebra-girl').css('z-index', ++env.maxIndex);
-            $('#zebra-girl').show();
-            $('#main-buffer').blur();
-            blurAllWindows();
-         }, 300);
+   jpTerminal.addCommand({
+         name: 'ls',
+         summary: 'list - list files in the current directory',
+         manPage: '',
+         command: function(env, inputLine) {
+         $('#main-input').append($('<div>zebraGirl.jpg</div>'));
       }
    });
 
-   jpTerminal.addCommand('help', function(env, inputLine) {
-      Object.keys(env.commands).sort().forEach(function(command) {
-         $('#' + env.active).find('.command-history').append($('<div>' + command + '</div>'));
-      });
+   jpTerminal.addCommand({
+         name: 'display',
+         summary: 'display - display image files. (hint: use ls to find a \'file\')',
+         manPage: '',
+         command: function(env, inputLine) {
+         var args = inputLine.trim().split(' ');
+
+         if (args.length < 2) {
+            $('#main-input').append($('<span>display: no file specified</span>'));
+            return;
+         }
+
+         if (inputLine.match(/zebraGirl\.jpg/)) {
+            setTimeout(function() {
+               $('#zebra-girl').css('z-index', ++env.maxIndex);
+               $('#zebra-girl').show();
+               $('#main-buffer').blur();
+               blurAllWindows();
+            }, 300);
+         }
+      }
+   });
+
+   jpTerminal.addCommand({
+      name: 'man',
+      summary: 'man - display reference manual for a given command',
+      manPage: 'USAGE\n   man [command name]',
+      command: function(env, inputLine) {
+         var arg = inputLine.trim().split(/ +/)[1] || '',
+             output = 'What manual page do you want?';
+
+         if (env.commands.hasOwnProperty(arg)) {
+            output = env.commands[arg].manPage;
+         } else if (arg) {
+            output = 'No manual entry for ' + $('<div/>').text(arg).html();
+         }
+
+         $('#main-input').append(output);
+      }
+   });
+
+   jpTerminal.addCommand({
+         name: 'help', 
+         summary: '',
+         manPage: '',
+         command: function(env, inputLine) {
+            for (var command in env.commands) {
+               $('#' + env.active).find('.command-history').append($('<div>' + env.commands[command].summary + '</div>'));
+            }
+      }
    });
 
    // helpers
